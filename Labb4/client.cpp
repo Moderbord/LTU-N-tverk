18827062235491fca98c2bcab926933eac819e49
Pontus Andersson <ponane-6@student.ltu.se>
2019-10-03 21:02:46 +0200

    Receiving answer from Linux server

--- Labb4/client.cpp
+++ Labb4/client.cpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "network.h"
-#include <winsock.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 #include <iostream>
 
 #define ADDRESS { 130, 240, 40, 7 }
@@ -8,43 +9,96 @@
 
 int main()
 {
+	spark::MsgHead head = { sizeof(spark::JoinMsg), 1, 1, spark::MsgType::JOIN };
+	spark::JoinMsg join = { head, spark::ObjectDesc::HUMAN, spark::ObjectForm::CONE, "sparky" };
 
-	WSADATA data;
-	WSAStartup(MAKEWORD(2,2), &data);
+	char mData[sizeof(join)];
+	memcpy(mData, &join, sizeof(join));
 
-	char message[] = "Heeeeej";
+	char mmData[sizeof(spark::JoinMsg)];
+	spark::Msg::Join("Sparky", 1, spark::ObjectDesc::HUMAN, spark::ObjectForm::CONE, mmData);
+	
 
-	sockaddr_in address;
+	WSADATA wsa;
+	SOCKET s;
+	struct sockaddr_in server;
+	//char message[] = "GET / HTTP/1.1\r\n\r\n";
+	char *server_reply = new char[2000];
+	int sent_size, recv_size;
 
-	WSADATA wsaData;
-
-	address.sin_family = AF_INET;
-	address.sin_addr = ADDRESS;
-	address.sin_port = htons(PORT);
-	//address.sin_flowinfo = 0;
-	//address.sin_scope_id = 0;
-
-	wsaData.iMaxSockets = 0;
-	wsaData.iMaxUdpDg = 0;
-	wsaData.lpVendorInfo = 0;
-	wsaData.wHighVersion = 0;
-	wsaData.wVersion = 0;
-
-
-	SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sock == INVALID_SOCKET) {
-
-		std::cout << WSAGetLastError() << std::endl; 
-	}
-
-	if (sendto(sock, message, 7, 0, (struct sockaddr*) & address, 32) == SOCKET_ERROR)
+	// Winsock init
+	std::cout << "Initializing Winsock.." << std::endl;
+	if (WSAStartup(MAKEWORD(2,2), &wsa) !=0)
 	{
-		std::cout << WSAGetLastError() << std::endl;
+		std::cout << "Failed: Error code : " << WSAGetLastError() << std::endl;
 	}
+	std::cout << "Initialized" << std::endl;
 
-	closesocket(sock);
+	// Socket init
+	std::cout << "\nCreating socket.." << std::endl;
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	{
+		std::cout << "Could not create socket : " << WSAGetLastError() << std::endl;
+	}
+	std::cout << "Socket created" << std::endl;
+
+	//Server init
+	std::cout << "\nInitializing socket data.." << std::endl;
+	const char addr[] = "130.240.40.7";			// Linux
+	//const char addr[] = "172.217.20.36";		// Google
+	if (inet_pton(AF_INET, addr, &(server.sin_addr))  != 1)
+	{
+		std::cout << "Failed: Error code : " << WSAGetLastError() << std::endl;
+	}
+	server.sin_family = AF_INET;
+	server.sin_port = htons(49152);				// Linux
+	//server.sin_port = htons(80);				// Google
+	std::cout << "Socket data initialied" << std::endl;
+
+	// Connect to remote server
+	std::cout << "\nConnection to server.." << std::endl;
+	if (connect(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		std::cout << "Connection error : " << WSAGetLastError() << std::endl;
+	}
+	std::cout << "Connected" << std::endl;
+
+	// Send data
+	std::cout << "\nSending data.." << std::endl;
+	if ((sent_size = send(s, mData, sizeof(mData), 0)) == SOCKET_ERROR)				// Linux
+	//if ((sent_size = send(s, message, strlen(message), 0)) == SOCKET_ERROR)		// Google
+	{
+		std::cout << "Error sending data : " << WSAGetLastError() << std::endl;
+	}
+	std::cout << "Sent " << sent_size << " bytes of data" << std::endl;
+
+	// Receive data
+	std::cout << "\nReceiving data.." << std::endl;
+	if ((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR)
+	{
+		std::cout << "Error receiving data : " << WSAGetLastError() << std::endl;
+	}
+	std::cout << "Received " << recv_size << " bytes of data" << std::endl << std::endl;
+
+	if ((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR)
+	{
+		std::cout << "Error receiving data : " << WSAGetLastError() << std::endl;
+	}
+	std::cout << "Received " << recv_size << " bytes of data" << std::endl << std::endl;
+
+
+	server_reply[recv_size-1] = '\0';
+
+	
+	std::cout << server_reply;
+
+
+
+	// Cleanup
+	closesocket(s);
 	WSACleanup();
-		 
 
 
+	delete[] server_reply;
+	server_reply = nullptr;
 }
--- Labb4/network.h
+++ Labb4/network.h
@@ -1,105 +1,128 @@
-//#pragma once
-//
-//namespace spark
-//{
-//
-//#define MAX_NAMELEN 32
-//#define DEF_BUFLEN 512
-//
-//	struct MsgHead
-//	{
-//		unsigned int length;	// Total length for whole message
-//		unsigned int seq_no;	// Sequence number
-//		unsigned int id;		// Client ID or 0
-//		MsgType type;			// Type of message
-//	};
-//
-//	struct JoinMsg
-//	{
-//		MsgHead head;
-//		ObjectDesc desc;
-//		//ClientForm form;
-//		char name[MAX_NAMELEN];
-//	};
-//
-//	struct LeaveMsg
-//	{
-//		MsgHead head;
-//	};
-//
-//	struct ChangeMsg
-//	{
-//		MsgHead head;
-//		ChangeType type;
-//	};
-//
-//	struct MewPlayerMessage
-//	{
-//		ChangeMsg msg;		//Change message header with new client id
-//		ObjectDesc desc;
-//		ObjectDesc form;
-//		char name[MAX_NAMELEN];
-//	};
-//
-//	struct PlayerLeaveMsg
-//	{
-//		ChangeMsg msg;		//Change message header with new client id
-//	};
-//
-//	struct NewPlayerPositionMsg
-//	{
-//		ChangeMsg msg;		//Change message header
-//		Coordinate pos;		//New object position 
-//		Coordinate dir;		//New object direction 
-//	};
-//
-//	struct Coordinate
-//	{
-//		int x;
-//		int y;
-//	};
-//
-//	struct EventMsg
-//	{
-//		MsgHead head;
-//		EventType type;
-//	};
-//
-//	enum MsgType
-//	{
-//		JOIN,			// Client joining game at server
-//		LEAVE,			// Client leaving game
-//		CHANGE,			// Information to clients
-//		EVENT,			// Information from clients to server
-//		TEXT_MESSAGE	// Send text messages to one or all
-//	};
-//
-//	enum ChangeType
-//	{
-//		NEW_PLAYER,
-//		PLAYER_LEAVE,
-//		NEW_PLAYER_POSITION
-//	};
-//
-//	enum ObjectDesc
-//	{
-//		HUMAN,
-//		NON_HUMAN,
-//		VEHICLE,
-//		STATIC_OBJECT
-//	};
-//
-//	enum ObjectForm
-//	{
-//		CUBE,
-//		SPHERE,
-//		PYRAMID,
-//		CONE
-//	};
-//
-//	enum EventType
-//	{
-//		MOVE
-//	};
-//
-//}
+#pragma once
+
+#include <cstring>
+#include <string>
+
+namespace spark
+{
+
+#define MAX_NAMELEN 32
+#define DEF_BUFLEN 512
+
+	enum MsgType
+	{
+		JOIN,			// Client joining game at server
+		LEAVE,			// Client leaving game
+		CHANGE,			// Information to clients
+		EVENT,			// Information from clients to server
+		TEXT_MESSAGE	// Send text messages to one or all
+	};
+
+	enum ChangeType
+	{
+		NEW_PLAYER,
+		PLAYER_LEAVE,
+		NEW_PLAYER_POSITION
+	};
+
+	enum ObjectDesc
+	{
+		HUMAN,
+		NON_HUMAN,
+		VEHICLE,
+		STATIC_OBJECT
+	};
+
+	enum ObjectForm
+	{
+		CUBE,
+		SPHERE,
+		PYRAMID,
+		CONE
+	};
+
+	enum EventType
+	{
+		MOVE
+	};
+
+	struct MsgHead
+	{
+		unsigned int length;	// Total length for whole message
+		unsigned int seq_no;	// Sequence number
+		unsigned int id;		// Client ID or 0
+		MsgType type;			// Type of message
+	};
+
+	struct JoinMsg
+	{
+		MsgHead head;
+		ObjectDesc desc;
+		ObjectForm form;
+		char name[MAX_NAMELEN];
+	};
+
+	struct LeaveMsg
+	{
+		MsgHead head;
+	};
+
+	struct ChangeMsg
+	{
+		MsgHead head;
+		ChangeType type;
+	};
+
+	struct NewPlayerMessage
+	{
+		ChangeMsg msg;		//Change message header with new client id
+		ObjectDesc desc;
+		ObjectDesc form;
+		char name[MAX_NAMELEN];
+	};
+
+	struct PlayerLeaveMsg
+	{
+		ChangeMsg msg;		//Change message header with new client id
+	};
+
+	struct Coordinate
+	{
+		int x;
+		int y;
+	};
+
+	struct NewPlayerPositionMsg
+	{
+		ChangeMsg msg;		//Change message header
+		Coordinate pos;		//New object position 
+		Coordinate dir;		//New object direction 
+	};
+
+	struct EventMsg
+	{
+		MsgHead head;
+		EventType type;
+	};
+
+
+
+	class Msg
+	{
+	public:
+		static const int seqNm = 1;
+
+		static void Join(const char name[], int id, ObjectDesc desc, ObjectForm form, char* buf)
+		{
+			MsgHead head = { sizeof(JoinMsg), seqNm, id, spark::MsgType::JOIN };
+			JoinMsg join = { head, desc, form, *(&name) };
+			
+			memcpy(buf, &join, sizeof(join));
+		}
+
+
+
+	};
+
+
+}
